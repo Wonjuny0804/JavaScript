e8a02c8fc52936ec913a1180e575de8e986068bd
WonjunJang <wonjun84@naver.com>
2020-11-04 10:18:41 +0900

    CodeUp 1 through 11 Solved

---
+++ codeUp100/6to11.js
@@ -0,0 +1,47 @@
+// number 6
+// print "!@#$%^&*()"
+console.log('"!@$%^&*()"');
+
+//number 7
+/* print the following address
+"C:\Download\hello.cpp"
+*/
+console.log('"C:\\Download\\hello.cpp"');
+
+//number 8
+/*
+print
+┌┬┐
+├┼┤
+└┴┘
+using unicode.
+*/
+
+console.log("\u250c\u252c\u2510\n\u251c\u253c\u2524\n\u2514\u2534\u2518");
+
+//number 9, well there is no number 9
+// number 10 instead..
+/*
+정수형(int)으로 변수를 선언하고, 변수에 정수값을 저장한 후
+변수에 저장되어 있는 값을 그대로 출력해보자.
+
+예시
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d", n);
+
+    return 0;
+}
+
+와 같은 형태로 가능하다.
+
+get one input value and print it.
+input 15
+output 15
+*/
+console.log(prompt());
+
+// note that is syntax can be also used to print other strings... 
+// which will cover number 11
