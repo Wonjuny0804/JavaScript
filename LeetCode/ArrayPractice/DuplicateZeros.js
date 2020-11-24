24c656f89c9ac0e352d3653d4f6498add03519a4
WonjunJang <wonjun84@naver.com>
2020-11-24 19:18:32 +0900

    Added new practices 201124

--- LeetCode/ArrayPractice/DuplicateZeros.js
+++ LeetCode/ArrayPractice/DuplicateZeros.js
@@ -1,1 +1,27 @@
-var sum;
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+
+var orgArr = [1,0,2,3,0,4,5,0];
+var duplicateZeros = function(arr) {
+    // first traverse from front to the end
+    var dupArr = [];
+    for (let i = 0; i < arr.length; i++ ){
+        if (arr[i] !== 0) {
+            dupArr.push(arr[i]);
+        }
+        else {
+            // 0을 두번 삽입한다. 
+            dupArr.push(arr[i]);
+            dupArr.push(arr[i]);
+        }
+    }
+    if (dupArr.length !== arr.length) {
+        dupArr = dupArr.slice(0,arr.length);
+    }
+    return dupArr;
+};
+
+var result = duplicateZeros(orgArr);
+console.log(result);
