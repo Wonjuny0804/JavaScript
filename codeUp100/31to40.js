1ffb0ef1fb734089119ccdda8455055270a6c8ae
WonjunJang <wonjun84@naver.com>
2020-11-25 21:46:48 +0900

    Feat: 34 solved

--- codeUp100/31to40.js
+++ codeUp100/31to40.js
@@ -1,7 +1,7 @@
 // 31번 
 // 10진수 정수 1개 입력받아 8진수로 출력하기
-intVal = Number(prompt());
-octVal = intVal.toSting(8);
+var intVal = Number(prompt());
+var octVal = intVal.toSting(8);
 console.log(octVal);
 
 // 32번
@@ -9,7 +9,7 @@ console.log(octVal);
 10진 정수 입력받아 16진수로 출력하기1(설명)
 */
 intVal = Number(prompt());
-hexVal = intVal.toString(16);
+var hexVal = intVal.toString(16);
 console.log(hexVal);
 
 // 33번 10진 정수 입력받아 16진수로 출력하기2(설명)
@@ -21,3 +21,7 @@ intVal = Number(prompt());
 hexVal = intVal.toString(16);
 hexVal += '';
 console.log(hexVal.toUpperCase());
+
+// 34번 8진 정수 1개 입력받아 10진수로 출력하기
+let value = Number(prompt());
+console.log(`${parseInt(value,8)}`);
