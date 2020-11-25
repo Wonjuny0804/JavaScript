69d796af629ad9ec699b8451a1c1d89f98116b25
WonjunJang <wonjun84@naver.com>
2020-11-25 21:36:55 +0900

    Feat: 32 sovled

--- codeUp100/31to40.js
+++ codeUp100/31to40.js
@@ -3,3 +3,11 @@
 intVal = Number(prompt());
 octVal = intVal.toSting(8);
 console.log(octVal);
+
+// 32번
+/*
+10진 정수 입력받아 16진수로 출력하기1(설명)
+*/
+intVal = Number(prompt());
+hexVal = intVal.toString(16);
+console.log(hexVal);
