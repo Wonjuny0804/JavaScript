4e1d2cb9e6eb5c16284478d4eb47ef465c1f583a
WonjunJang <wonjun84@naver.com>
2020-11-30 12:53:32 +0900

    Create: BinarySearch.js

---
+++ practice/binarysearch.js
@@ -0,0 +1,29 @@
+
+function BinarySearch(array, target) {
+  array.sort();
+  while (array.length > 0) {
+    if (array.length % 2) {
+      var i = Math.round(array.length / 2);
+      if (array[i] > target) {
+        array = array.slice(0, i);
+        console.log(array);
+      } else if (array[i] < target) {
+        array = array.slice(i + 1, array.length);
+        console.log(array);
+      } else {
+        return i;
+      }
+    } else {
+      var j = array.length / 2;
+      if (array[j] > target) {
+        array = array.slice(0, j);
+      } else if (array[j] < target) {
+        array = array.slice(j + 1, array.length);
+        console.log(array);
+      } else {
+        return j;
+      }
+    }
+  }
+  return -1;
+}
