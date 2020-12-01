8ac61f2f3fc775105644324b19315e264af92487
WonjunJang <wonjun84@naver.com>
2020-12-01 21:50:31 +0900

    Feat modified binarySearch.js

--- practice/binarysearch.js
+++ practice/binarysearch.js
@@ -3,7 +3,7 @@ function BinarySearch(array, target) {
   array.sort();
   while (array.length > 0) {
     if (array.length % 2) {
-      var i = Math.round(array.length / 2);
+      var i = Math.floor(array.length / 2);
       if (array[i] > target) {
         array = array.slice(0, i);
         console.log(array);
@@ -27,4 +27,13 @@ function BinarySearch(array, target) {
   }
   return -1;
 }
+
+
 // 현재 작동 안함... 디버깅 중...
+console.log(BinarySearch([1, 2, 3, 4, 5, 6], 1)); // 0
+console.log(BinarySearch([1, 2, 3, 4, 5, 6], 3)); // 2
+console.log(BinarySearch([1, 2, 3, 4, 5, 6], 5)); // 4
+// console.log(BinarySearch([1, 2, 3, 4, 5, 6], 6)); // 5
+// console.log(BinarySearch([1, 2, 3, 4, 5, 6], -1)); // -1
+// console.log(BinarySearch([1, 2, 3, 4, 5, 6], 0)); // -1
+// console.log(BinarySearch([1, 2, 3, 4, 5, 6], 7)); // -1
