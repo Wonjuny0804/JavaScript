b71a2bfd5c9e8a843208ec42cc8653cd78bf9572
WonjunJang <wonjun84@naver.com>
2020-12-02 22:35:46 +0900

    Feat: 41to50.js completed

--- codeUp100/41to50.js
+++ codeUp100/41to50.js
@@ -35,4 +35,40 @@ const autoCal = function (num1, num2) {
   console.log(Math.floor(num1/num2));
   console.log(num1%num2);
   console.log(num1/num2);
-}
+};
+
+// 46 정수 3개 입력받아 합과 평균 출력하기
+const threeNumbers = function (num1, num2, num3) {
+  const sum = num1 + num2 + num3; 
+  console.log(sum);
+  console.log(sum/3);
+};
+
+// 47번 정수 1개 입력받아 2배 곱해 출력하기 비트 연산자 << 사용
+const doublebit = function (num) {
+  console.log(num<<1);
+};
+
+// 48번 한 번에 2의 거듭제곱 배로 출력하기(설명)
+const square = function (num1, num2) {
+  console.log(num1**num2);
+};
+
+// 49번 두 정수 입력받아 비교하기1(설명)
+/*
+두 정수(a, b)를 입력받아
+
+a가 b보다 크면 1을, a가 b보다 작거나 같으면 0을 출력하는 프로그램을 작성해보자.
+*/
+const compare = function(num1, num2) {
+  return result = num1 > num2 ? 1 : 0;
+};
+
+// 50번 두 정수 입력받아 비교하기2(설명)
+/*
+두 정수(a, b)를 입력받아
+a와 b가 같으면 1을, 같지 않으면 0을 출력하는 프로그램을 작성해보자.
+*/
+const compare = function(num1, num2) {
+  return num1 === num2 ? 1 : 0;
+};
