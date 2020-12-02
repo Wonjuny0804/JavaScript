8fd7f8034b6ccdff516c72f9702134f44bd2ce1c
WonjunJang <wonjun84@naver.com>
2020-12-02 20:08:29 +0900

    Feat: solved, but need improvement

--- LeetCode/ArrayPractice/MoveZeroes.js
+++ LeetCode/ArrayPractice/MoveZeroes.js
@@ -8,7 +8,8 @@ Notes
 You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 */
-
+// first version 
+/*
 var moveZeroes = function(nums) {
     let writePointer = 0;
     for(let readPointer = 0; readPointer < nums.length; readPointer++){
@@ -20,3 +21,18 @@ var moveZeroes = function(nums) {
       }
     }
 };
+*/
+var moveZeroes = function(nums) {
+  let result = [];
+  let count = 0;
+  for (let i = 0; i < nums.length; i++){
+    if (nums[i] !== 0) result.push(nums[i]);
+    else count++;
+  }
+  console.log(count);
+  for ( let j = 0; j < count; j++){
+    result.push(0);
+  }
+  return result;
+};
+console.log(moveZeroes([0,1,0,3,12]));
