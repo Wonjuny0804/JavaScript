f4d6d7c0b9b255c75f2dbc3a4e9bc3eabfbc2341
WonjunJang <wonjun84@naver.com>
2020-12-10 18:10:13 +0900

    Feat: spacing according to ESlint

--- practice/bubbleSort.js
+++ practice/bubbleSort.js
@@ -4,7 +4,7 @@ const bubbleSort = function (arr) {
   while (count < arr.length) {
     for (let i = 0; i < arr.length; i++) {
       if (arr[i] > arr[i + 1]) {
-        temp = arr[i+1];
+        temp = arr[i + 1];
         arr[i + 1] = arr[i];
         arr[i] = temp;
       }
@@ -13,4 +13,4 @@ const bubbleSort = function (arr) {
   }
   return arr;
 };
-console.log(bubbleSort([2,4,5,1,3]));
+console.log(bubbleSort([2, 4, 5, 1, 3]));
