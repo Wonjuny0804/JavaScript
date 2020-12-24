7df3a7cb2e1514efe5e70e955751653be12e09ea
WonjunJang <wonjun84@naver.com>
2020-12-24 13:37:08 +0900

    Feat: 7.js completed

---
+++ practice/HOR_Practice/7.js
@@ -0,0 +1,28 @@
+let todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+
+// function toggleCompletedAll() {
+//   todos.forEach(todo => {
+//     todo["completed"] = true;
+//   });
+// }
+
+function toggleCompletedAll() {
+  todos.forEach((todo, index) => {
+    todos[index] = { ...todo, completed: true};
+  });
+}
+
+toggleCompletedAll();
+
+console.log(todos);
+/*
+[
+  { id: 3, content: 'HTML', completed: true },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: true }
+]
+*/
