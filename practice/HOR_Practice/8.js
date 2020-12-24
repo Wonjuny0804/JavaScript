99a16322a574cbbd4123f9357724acc9f98af4e8
WonjunJang <wonjun84@naver.com>
2020-12-24 13:46:20 +0900

    Feat: 8.js added reduce solution

--- practice/HOR_Practice/8.js
+++ practice/HOR_Practice/8.js
@@ -4,8 +4,10 @@ let todos = [
   { id: 1, content: 'Javascript', completed: false }
 ];
 
-function countCompletedTodos() {
-  return todos.filter(todo => todo.completed).length;
-}
+// function countCompletedTodos() {
+//   return todos.filter(todo => todo.completed).length;
+// }
+
+const countCompletedTodos = () => todos.reduce((acc, cur) => (cur.completed ? acc + 1 : acc), 0);
 
 console.log(countCompletedTodos()); // 1
