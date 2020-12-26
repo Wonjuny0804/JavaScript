ae8d45efc7b6466e2e9f02118873a9e52bec3a60
WonjunJang <wonjun84@naver.com>
2020-12-26 22:54:58 +0900

    style: Change derectory

--- practice/11.js
+++
@@ -1,6 +0,0 @@
-// 삼각형 출력하기
-var star = '*';
-for ( var i = 0 ; i < 5; i++ ){
-  console.log(star);
-  star += '*';
-}
--- practice/13.js
+++
@@ -1,8 +0,0 @@
-star = '';
-for (var j = 0; j < 5; j++){
-  for ( var  i = 5-j; i > 0; i-- ){
-    star += '*';
-  }
-  console.log(star);
-  star = '';
-}
