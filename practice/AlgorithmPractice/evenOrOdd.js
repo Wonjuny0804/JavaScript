b07921ccbcaabf4d84988f3e1721050ff0c3fded
WonjunJang <wonjun84@naver.com>
2020-12-26 23:21:19 +0900

    style: fix according to ESLint

--- practice/AlgorithmPractice/evenOrOdd.js
+++ practice/AlgorithmPractice/evenOrOdd.js
@@ -2,14 +2,14 @@
 function evenOrOdd(num) {
   if (num % 2) {
     return 'Odd';
-  } else {return 'Even'; }
+  }
+  return 'Even';
 }
 // 삼항 조건 연산자
 function evenOrOdd2(num) {
   return num % 2 ? 'Odd' : 'Even';
 }
 
-
-console.log(evenOrOdd2(2)); // Even
+console.log(evenOrOdd(2)); // Even
 console.log(evenOrOdd2(3)); // Odd
 console.log(evenOrOdd2(1000)); // Even
--- practice/AlgorithmPractice/getCount8.js
+++ practice/AlgorithmPractice/getCount8.js
@@ -1,6 +1,6 @@
 /*
 1~10000의 숫자 중 8이 등장하는 회숫 구하기
-*/ 
+*/
 const getCount8 = () => {
   let count = 0;
   for (let i = 0; i < 10000; i++) {
