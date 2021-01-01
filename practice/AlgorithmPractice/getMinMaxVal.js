daabce5c626eb5209822fa9f2947bd7b2e9dcfc1
WonjunJang <wonjun84@naver.com>
2021-01-01 22:54:41 +0900

    feat: Complete problem

---
+++ practice/AlgorithmPractice/getMinMaxVal.js
@@ -0,0 +1,5 @@
+const getMaxValueFromArray = array => Math.max(...array);
+console.log(getMaxValueFromArray([3, 6, -2, -5, 7, 3])); // 7
+
+const getMinValueFromArray = array => Math.min(...array);
+console.log(getMinValueFromArray([3, 6, -2, -5, 7, 3])); // -5
