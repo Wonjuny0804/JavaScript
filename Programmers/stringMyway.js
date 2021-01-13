0e0d828b86c57ad3def03b46488d7fc941600a7b
WonjunJang <wonjun84@naver.com>
2021-01-13 23:38:14 +0900

    feat: Complete solution

--- Programmers/stringMyway.js
+++ Programmers/stringMyway.js
@@ -1,9 +1,12 @@
 function solution(strings, n) {
   var answer = [];
-  answer = [...strings].map(item => item.slice(n));
-  answer.sort();
-  answer = answer.map(item => strings.filter(string => string.slice(n) === item)).flat();
+  answer = [...strings].sort((string1, string2) => {
+    if (string1.slice(n) > string2.slice(n)) return 1;
+    else if (string1.slice(n) < string2.slice(n)) return -1;
+    else return 0;
+  });
   return answer;
 }
-console.log(solution(	["sun", "bed", "car"], 1));
+console.log(solution(["sun", "bed", "car"], 1));
+console.log(solution(['abce', 'abcd', 'cdx']));
 console.log('ed' - 'un');
