49226d6587266995b144ec42a35148656b0f22ec
WonjunJang <wonjun84@naver.com>
2021-01-14 11:37:37 +0900

    feat: Complete solution

--- Programmers/strChk.js
+++ Programmers/strChk.js
@@ -1,12 +1,7 @@
 function solution(s) {
-  var answer = true;
-  const str = Array.from(s);
-  console.log(s.length);
-  if (s.length !== 4 || s.length !== 6) return false;
-  for (let i = 0; i < str.length; i++) {
-    console.log(str[i]);
-  }
+  const answer = true;
+  if (s.length !== 4 && s.length !== 6) return false;
+  for (let i = 0; i < s.length; i++) if (Number.isNaN(+s[i])) return false;
   return answer;
 }
-console.log(solution('1234'));
-console.log(typeof ('1' * 0));
+console.log(solution('a234'));
