832663e86a218e9bdb3fe9a52beb84836c82a794
WonjunJang <wonjun84@naver.com>
2021-01-15 11:38:42 +0900

    feat: Complete solution

---
+++ Programmers/ceaser.js
@@ -0,0 +1,10 @@
+const s = 'a B z';
+const n = 4;
+const str = Array.from(s);
+const answer = str.map(ss => {
+  if (ss === ' ') return ' '; 
+  const ascii = ss.charCodeAt(0) < 91 ? ss.charCodeAt(0) + n > 90 ? ss.charCodeAt(0) + n - 26 : ss.charCodeAt(0) + n : ss.charCodeAt(0) + n > 122 ? ss.charCodeAt(0) + n - 26 : ss.charCodeAt(0) + n;
+  return String.fromCharCode(ascii);
+}).join('');
+
+console.log(answer);
