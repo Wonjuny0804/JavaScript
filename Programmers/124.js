c4e0c9af97df4fe96e666bd6645b0fcf8dcd6f94
WonjunJang <wonjun84@naver.com>
2021-05-10 07:21:07 +0900

    resolve Complete question

--- Programmers/124.js
+++ Programmers/124.js
@@ -1,3 +1,24 @@
-function solution(n) {
-  
+function solution(num) {
+  let remainder = 0;
+  let value = num;
+  let answer = '';
+
+  while (value > 0) {
+    remainder = value % 3;
+    value = Math.floor(value / 3);
+
+    if (remainder === 0) {
+      value -= 1;
+      remainder = 4;
+    }
+
+    answer = remainder + answer;
+  }
+
+  return answer;
 }
+
+console.log(solution(6)); // 14
+console.log(solution(12)); // 21
+console.log(solution(18)); // 112
+console.log(solution(21)); // 211
